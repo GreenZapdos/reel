49dde1d8e4a8f1ed6108bf6bbea0f0d7cbd81ffb
GreenZapdos <green.zapdos@gmail.com>
2014-04-25 22:18:55 -0600

    Added dynamic resizing and caching of movie posters.

--- reel.py
+++ reel.py
@@ -1,6 +1,6 @@
 ##Configuration##
 movie_path = '/path/to/movies/' #Full path of movie folder goes here
-port = 9000
+port = 9001
 debug = True
 
 if movie_path == '/path/to/movies/':
@@ -8,9 +8,10 @@ if movie_path == '/path/to/movies/':
 	exit()
 
 from flask import Flask, request, send_file, Response, render_template, url_for
-import os, mimetypes, re
+import os, mimetypes, re, StringIO, datetime, user_agents
 from itertools import izip_longest
 from bs4 import BeautifulSoup
+from PIL import Image
 app = Flask(__name__)
 
 def settings(s):
@@ -18,12 +19,6 @@ def settings(s):
 		return movie_path
 	if s == 'port':
 		return port
-
-
-@app.after_request
-def after_request(response):
-    response.headers.add('Accept-Ranges', 'bytes')
-    return response
 
 def find_movies(path):
 	list = []
@@ -40,7 +35,7 @@ def find_movies(path):
 					for f in filenames:
 						split = os.path.splitext(f)
 						if split[1] == '.tbn' or split[1] == '.jpg' or split[1] == '.png':
-							movie['tbn'] = os.path.join(dirpath, f).replace(movie_path, '/movie-directory/')
+							movie['tbn'] = os.path.join(dirpath, f).replace(movie_path, '/posters/')
                                                 if split[1] == '.nfo':
 							try:
 								soup = BeautifulSoup(open(os.path.join(dirpath, f)))
@@ -61,6 +56,34 @@ def movies():
 	movie_list = list(izip_longest(movie_list[0::3], movie_list[1::3], movie_list[2::3]))
 	return render_template('index.html', movie_list=movie_list)
 
+@app.route('/posters/<path:filename>')
+def img(filename):
+	modified = datetime.datetime.fromtimestamp(os.path.getmtime(movie_path + filename)).strftime('%a, %d %b %Y %H:%M:%S')
+	if 'If-Modified-Since' in request.headers and request.headers['If-Modified-Since'] == modified:
+		return  Response(response='', status=304, headers={'Cache-Control': 'max-age=31557600, public', 'Last-Modified': modified}, mimetype='image/jpeg', content_type=None, direct_passthrough=False)
+	else:
+		tbn = Image.open(movie_path + filename)
+		if user_agents.parse(request.headers.get('User-Agent')).is_mobile:
+			multiplier = 1
+		else:
+			multiplier = 2
+		if tbn.size[0] / tbn.size[1] > 2/3:
+			baseheight = 400*multiplier
+			hpercent = (baseheight / float(tbn.size[1]))
+			wsize = int((float(tbn.size[0]) * float(hpercent)))
+			tbn = tbn.resize((wsize, baseheight), Image.ANTIALIAS)
+			tbn = tbn.crop((0, 0, 266*multiplier, 400*multiplier)) 
+			output = StringIO.StringIO()
+			tbn.save(output, format='JPEG')
+		else:
+			basewidth = 266*multiplier
+			wpercent = (basewidth / float(tbn.size[0]))
+			hsize = int((float(tbn.size[1]) * float(wpercent)))
+			tbn = tbn.resize((basewidth, hsize), Image.ANTIALIAS)
+			tbn = tbn.crop((0, 0, 266*multiplier, 400*multiplier)) 
+			output = StringIO.StringIO()
+			tbn.save(output, format='JPEG')
+		return Response(response=[output.getvalue()], status=200, headers={'Cache-Control': 'max-age=31557600, public', 'Last-Modified': modified}, mimetype='image/jpeg', content_type=None, direct_passthrough=False)
 
 if __name__ == '__main__':
 	print 'run server.py not reel.py'
